214e09ccbbbcfce1be38e6064b6e91c0298b3a56
Tech Wolf <104517180+miclegoden@users.noreply.github.com>
2023-03-03 18:10:19 +0900

    fix

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -2,84 +2,38 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+
 export default function Landing() {
-  const [email, setEmail] = useState("");
-  const handleRegister = async (event: { preventDefault: () => void }) => {
-    event.preventDefault();
-    console.log("click", email);
-
-    let response = await axios.get(
-      `http://localhost:8080/api/register?email=${email}`
-    );
-    setEmail("");
-
-    // const response = await fetch("http://localhost:8080/api/register", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify({ email })
-    // });
-
-    const data = response.data;
-    console.log(data);
-  };
   return (
-    <div className="flex justify-items-center items-center space-x-6 mx-auto">
-      <div className="flex flex-col space-y-6">
-        <p className="animate-bounce text-7xl font-extrabold dark:text-white font-bold font-mono">
-          Launch Gen
-        </p>
-        {/* <img src="/img/landing.png" alt="landing" /> */}
-        <p className="text-2xl  dark:text-white font-bold">
-          Vercel is the platform for frontend developers, providing the speed
-          <br />
-          and reliability innovators need to create at the moment of
-          inspiration.
+    <div className="flex justify-items-center items-center mt-48 mx-auto">
+      <div className="flex flex-col justify-items-center items-center space-y-6">
+        <p className="text-5xl font-extrabold dark:text-white font-bold font-sans">
+          Create your custom landing pages
+          <span className="text-indigo-500"> using AI</span>
         </p>
 
-        <form className="w-[50%]" onSubmit={handleRegister}>
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-              </svg>
-            </div>
-            <input
-              type="email"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-              id="search"
-              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@lauchgen.com"
-              required
-            />
-            <button
-              type="submit"
-              className="border-2 border-indigo-600 text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        <div>
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Send email
-            </button>
-          </div>
-        </form>
-        <div className="space-x-4">
-          <button className="border-2 border-indigo-600 rounded-lg px-6 py-4 bg-black text-white font-bold">
-            Lorem Ipusum
-          </button>
-          <button className="  border-2 border-indigo-600 rounded-lg px-6 py-4 bg-white  font-bold">
-            Lorem Ipusum
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M11.757 2.03399C12.034 2.10699 12.266 2.29597 12.395 2.55297C12.878 3.51997 13.239 4.107 13.602 4.583C13.97 5.065 14.358 5.45898 14.95 6.04998C16.316 7.41698 17 9.20999 17 11C17 12.79 16.316 14.583 14.95 15.95C12.216 18.683 7.784 18.683 5.05 15.95C3.684 14.583 3 12.79 3 11C3 9.20999 3.684 7.41698 5.05 6.04998C5.336 5.76398 5.766 5.67898 6.14 5.83398C6.514 5.98798 6.757 6.35301 6.757 6.75701C6.757 7.87701 6.827 8.73 7.155 9.411C7.335 9.785 7.61601 10.151 8.10001 10.478C8.21601 9.41702 8.428 8.124 8.714 6.898C8.939 5.932 9.21899 4.96799 9.55299 4.16399C9.71999 3.76099 9.909 3.37897 10.123 3.04797C10.331 2.72597 10.599 2.39902 10.945 2.16802C11.184 2.00902 11.48 1.95999 11.757 2.03399ZM12.121 15.121C10.95 16.293 9.05 16.293 7.879 15.121C7.293 14.536 7 13.768 7 13C7 13 7.879 13.5 9.5 13.5C9.5 12.5 10 9.49999 10.75 8.99999C11.25 9.99999 11.536 10.293 12.121 10.879C12.707 11.464 13 12.232 13 13C13 13.768 12.707 14.536 12.121 15.121Z"
+                fill="white"
+              />
+            </svg>
+            Get Started
           </button>
         </div>
-      </div>
-      <div>
-        <img src="/img/landing.png" alt="landing" className="max-md:hidden" />
       </div>
     </div>
   );
--- components/LandingContainer/index.tsx
+++ components/LandingContainer/index.tsx
@@ -2,146 +2,87 @@
 /* eslint-disable @next/next/no-img-element */
 export default function LandingContainer() {
   return (
-    <div className="lg:flex flex-col mt-48 px-14 md:mt-24 sm: mt-12 ">
-      <div className="flex flex-wrap  mb-12">
-        <div className="lg:flex justify-between ">
-          <div className="flex flex-col ">
-            <h1 className="lg:text-[65px] font-bold mb-6 md: text-[28px] md:font-[900]">
-              What Makes Us Better <br />{" "}
-              <span className="text-main">From Others</span>
-            </h1>
-            <h1 className="text-gray-600 text-xl mt-5">
-              With our 24/7 customer support, real time updates on your project,
-              our middleman service to ensure the security of your funds, and an
-              open and welcoming community of freelancers - you can be sure that
-              your project is in safe hands.
-            </h1>
-          </div>
-          <div className="grid justify-items-stretch lg:flex flex-wrap">
-            <div className="lg:inline-grid grid-cols-2 gap-4 md:flex flex-col md: space-y-4 w-full">
-              <div className="flex flex-col lg:mt-5  md:mt-5">
-                <div className="flex items-center mb-3 h-[56px]">
-                  <img
-                    src="/icons/Group 1000001088.png"
-                    alt="icon"
-                    className="w-14 -translate-x-2"
-                  />
-                  <h1 className="text-2xl font-bold text-main-gray">
-                    Growing Fast!
-                  </h1>
-                </div>
-                <h1 className=" text-gray-600 pt-0 pl-14">
-                  Our community of more than 5,000 freelancers covers multiple
-                  disciplines.
-                </h1>
-              </div>
-              <div className="flex flex-col mt-5 md:mt-0">
-                <div className="flex items-center mb-3 h-[56px] ">
-                  <img src="/icons/like.png" alt="icon" className="w-7 mr-3" />
-                  <h1 className="text-2xl font-bold text-main-gray">
-                    100% Satisfaction
-                  </h1>
-                </div>
-                <h1 className=" text-gray-600 pl-14">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                  et mattis dolor, ut egestas risus.
-                </h1>
-              </div>
-              <div className="flex flex-col  mt-6">
-                <div className="flex items-center mb-3 h-[56px]">
-                  <img
-                    src="/icons/people.png"
-                    alt="icon"
-                    className="w-7 mr-3"
-                  />
-                  <h1 className="text-2xl font-bold text-main-gray">
-                    Expert Team Members
-                  </h1>
-                </div>
-                <h1 className=" text-gray-600 pl-14">
-                  Our multi discipline team are experts in their fields,
-                  producing only the highest quality of work for an affordable
-                  price.
-                </h1>
-              </div>
-              <div className="flex flex-col mt-5">
-                <div className="flex items-center mb-3 h-[56px]">
-                  <img
-                    src="/icons/ranking.png"
-                    alt="icon"
-                    className="w-7 mr-3"
-                  />
-                  <h1 className="text-2xl font-bold text-main-gray">
-                    Welcoming Community
-                  </h1>
-                </div>
-                <h1 className="text-gray-600 pl-14">
-                  Our expert community is always happy to help, direct and
-                  advise to help you along the way to a successful project!
-                </h1>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col w-full">
-          <div className="lg:flex  justify-between  mt-10 ">
-            <div className="flex">
-              <img
-                src="/images/whyvanta.png"
-                className="z-30 lg:w-[600px] mr-20 md: w-full"
+    <div className="flex flex-col mt-48 px-14 md:mt-24 sm: mt-12 ">
+      <div className="flex flex-col justify-items-center items-center space-y-6">
+        <h1 className="text-4xl font-bold mb-6 md: text-[28px] md:font-[900]">
+          Why choose us? <br />{" "}
+        </h1>
+
+        <div className="flex space-x-20">
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <svg
+              width="40"
+              height="41"
+              viewBox="0 0 40 41"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6.24997 23.469L6.49995 23.3247V23.036V8.5C6.49995 8.10218 6.65798 7.72064 6.93929 7.43934C7.22059 7.15804 7.60212 7 7.99995 7C8.39777 7 8.7793 7.15804 9.06061 7.43934C9.34191 7.72064 9.49995 8.10218 9.49995 8.5V23.036V23.3247L9.74992 23.469C10.2819 23.7762 10.7237 24.218 11.0309 24.7501C11.3381 25.2821 11.4998 25.8857 11.4998 26.5C11.4998 27.1143 11.3381 27.7179 11.0309 28.2499C10.7237 28.782 10.2819 29.2238 9.74992 29.531L9.49995 29.6753V29.964V32.5C9.49995 32.8978 9.34191 33.2794 9.06061 33.5607C8.7793 33.842 8.39777 34 7.99995 34C7.60212 34 7.22059 33.842 6.93929 33.5607C6.65798 33.2794 6.49995 32.8978 6.49995 32.5V29.964V29.6753L6.24997 29.531C5.71794 29.2238 5.27615 28.782 4.96899 28.2499C4.66183 27.7179 4.50012 27.1143 4.50012 26.5C4.50012 25.8857 4.66183 25.2821 4.96899 24.7501C5.27615 24.218 5.71794 23.7762 6.24997 23.469ZM18.25 11.469L18.4999 11.3247V11.036V8.5C18.4999 8.10218 18.658 7.72064 18.9393 7.43934C19.2206 7.15804 19.6021 7 19.9999 7C20.3978 7 20.7793 7.15804 21.0606 7.43934C21.3419 7.72064 21.4999 8.10218 21.4999 8.5V11.036V11.3247L21.7499 11.469C22.2819 11.7762 22.7237 12.218 23.0309 12.7501C23.3381 13.2821 23.4998 13.8857 23.4998 14.5C23.4998 15.1143 23.3381 15.7179 23.0309 16.2499C22.7237 16.782 22.2819 17.2238 21.7499 17.531L21.4999 17.6753V17.964V32.5C21.4999 32.8978 21.3419 33.2794 21.0606 33.5607C20.7793 33.842 20.3978 34 19.9999 34C19.6021 34 19.2206 33.842 18.9393 33.5607C18.658 33.2794 18.4999 32.8978 18.4999 32.5V17.964V17.6753L18.25 17.531C17.7179 17.2238 17.2762 16.782 16.969 16.2499C16.6618 15.7179 16.5001 15.1143 16.5001 14.5C16.5001 13.8857 16.6618 13.2821 16.969 12.7501C17.2762 12.218 17.7179 11.7762 18.25 11.469ZM33.4999 23.036V23.3247L33.7499 23.469C34.2819 23.7762 34.7237 24.218 35.0309 24.7501C35.3381 25.2821 35.4998 25.8857 35.4998 26.5C35.4998 27.1143 35.3381 27.7179 35.0309 28.2499C34.7237 28.782 34.2819 29.2238 33.7499 29.531L33.4999 29.6753V29.964V32.5C33.4999 32.8978 33.3419 33.2794 33.0606 33.5607C32.7793 33.842 32.3978 34 31.9999 34C31.6021 34 31.2206 33.842 30.9393 33.5607C30.658 33.2794 30.4999 32.8978 30.4999 32.5V29.964V29.6753L30.25 29.531C29.7179 29.2238 29.2761 28.782 28.969 28.2499C28.6618 27.7179 28.5001 27.1143 28.5001 26.5C28.5001 25.8857 28.6618 25.2821 28.969 24.7501C29.2761 24.218 29.7179 23.7762 30.25 23.469L30.4999 23.3247V23.036V8.5C30.4999 8.10218 30.658 7.72064 30.9393 7.43934C31.2206 7.15804 31.6021 7 31.9999 7C32.3978 7 32.7793 7.15804 33.0606 7.43934C33.3419 7.72064 33.4999 8.10217 33.4999 8.5V23.036Z"
+                fill="#5850EC"
+                stroke="#5850EC"
               />
-            </div>
-            <div className="flex flex-col lg:ml-20 mt-20 md:ml-10">
-              <h1 className="text-main-gray font-bold lg:text-[54px] md: text-[28px]">
-                Why Are People Choosing LaunchGen?
-              </h1>
-              <div className="flex items-center mt-4">
-                <img
-                  src="/icons/user-tick.png"
-                  alt="user"
-                  className="my-4 mr-4"
-                />
-                <div className="flex flex-col">
-                  <h1 className="text-2xl font-bold text-main-gray">
-                    Integrity
-                  </h1>
-                  <h1 className="text-gray-600 max-w-[500px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Etiam et mattis dolor
-                  </h1>
-                </div>
-              </div>
-              <div className="flex items-center mt-4">
-                <img
-                  src="/icons/medal-star.png"
-                  alt="medal-star"
-                  className="my-4 mr-4"
-                />
-                <div className="flex flex-col">
-                  <h1 className="text-2xl font-bold text-main-gray">Trust</h1>
-                  <h1 className="text-gray-600 max-w-[500px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Etiam et mattis dolor
-                  </h1>
-                </div>
-              </div>
-              <div className="flex items-center mt-4">
-                <img
-                  src="/icons/ranking2.png"
-                  alt="medal-star"
-                  className="my-4 mr-4"
-                />
-                <div className="flex flex-col">
-                  <h1 className="text-2xl font-bold text-main-gray">
-                    Excellence
-                  </h1>
-                  <h1 className="text-gray-600 max-w-[500px]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Etiam et mattis dolor
-                  </h1>
-                </div>
-              </div>
-            </div>
+            </svg>
+
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Customized
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              Bored of using free Wordpress templates? Or just reusing the same
+              shopify app? This is for you
+            </p>
+          </div>
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <svg
+              width="40"
+              height="40"
+              viewBox="0 0 40 40"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M17.5 20V20.0001C17.5001 20.6631 17.7636 21.2989 18.2325 21.7676L23.8884 27.4255L23.8884 27.4256C24.1207 27.6578 24.3964 27.842 24.6999 27.9677C25.0033 28.0934 25.3286 28.1581 25.657 28.1581C25.9854 28.1581 26.3107 28.0934 26.6141 27.9677C26.9176 27.842 27.1933 27.6578 27.4256 27.4256C27.6578 27.1933 27.842 26.9176 27.9677 26.6141C28.0934 26.3107 28.1581 25.9854 28.1581 25.657C28.1581 25.3286 28.0934 25.0033 27.9677 24.6999C27.842 24.3964 27.6578 24.1207 27.4256 23.8884L27.4255 23.8884L22.5 18.9648V12C22.5 11.337 22.2366 10.7011 21.7678 10.2322C21.2989 9.76339 20.663 9.5 20 9.5C19.337 9.5 18.7011 9.76339 18.2322 10.2322C17.7634 10.7011 17.5 11.337 17.5 12V20ZM30.9602 30.9602C28.0533 33.867 24.1109 35.5 20 35.5C15.8891 35.5 11.9467 33.867 9.03984 30.9602C6.13303 28.0533 4.5 24.1109 4.5 20C4.5 15.8891 6.13303 11.9467 9.03984 9.03984C11.9467 6.13303 15.8891 4.5 20 4.5C24.1109 4.5 28.0533 6.13303 30.9602 9.03984C33.867 11.9467 35.5 15.8891 35.5 20C35.5 24.1109 33.867 28.0533 30.9602 30.9602Z"
+                fill="#E3A008"
+                stroke="#E3A008"
+              />
+            </svg>
+
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Quick
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              Don’t wait for your designers to finish their work and developers
+              to finish the app. Do it instantly!
+            </p>
+          </div>
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <svg
+              width="40"
+              height="41"
+              viewBox="0 0 40 41"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8 12H7.5V12.5V23.9641C6.75661 23.8568 6.06249 23.5122 5.52513 22.9749C4.86875 22.3185 4.5 21.4283 4.5 20.5V12.5C4.5 11.5717 4.86875 10.6815 5.52513 10.0251C6.1815 9.36875 7.07174 9 8 9H24C24.9283 9 25.8185 9.36875 26.4749 10.0251C27.0122 10.5625 27.3568 11.2566 27.4641 12H8ZM12.5 20.5C12.5 19.5717 12.8687 18.6815 13.5251 18.0251C14.1815 17.3687 15.0717 17 16 17H32C32.9283 17 33.8185 17.3687 34.4749 18.0251C35.1313 18.6815 35.5 19.5717 35.5 20.5V28.5C35.5 29.4283 35.1313 30.3185 34.4749 30.9749C33.8185 31.6313 32.9283 32 32 32H16C15.0717 32 14.1815 31.6313 13.5251 30.9749C12.8687 30.3185 12.5 29.4283 12.5 28.5V20.5ZM24 29C25.1935 29 26.3381 28.5259 27.182 27.682C28.0259 26.8381 28.5 25.6935 28.5 24.5C28.5 23.3065 28.0259 22.1619 27.182 21.318C26.3381 20.4741 25.1935 20 24 20C22.8065 20 21.6619 20.4741 20.818 21.318C19.9741 22.1619 19.5 23.3065 19.5 24.5C19.5 25.6935 19.9741 26.8381 20.818 27.682C21.6619 28.5259 22.8065 29 24 29Z"
+                fill="#0E9F6E"
+                stroke="#0E9F6E"
+              />
+            </svg>
+
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Cheap
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-500 dark:text-gray-400">
+              Bored of using free Wordpress templates? Or just reusing the same
+              shopify app? This is for you
+            </p>
           </div>
         </div>
       </div>
